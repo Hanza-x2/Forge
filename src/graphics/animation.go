dd5e651cd49597e2ba527f8fe6e38e3c596267e7
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2025-04-14 23:13:14 +0200

    Integrated graphics.Animation and graphics.Shader types.

---
+++ src/graphics/animation.go
@@ -0,0 +1,38 @@
+package graphics
+
+type Animation struct {
+	Frames        []*TextureRegion
+	FrameDuration float32
+	Loop          bool
+}
+
+func NewAnimation(frames []*TextureRegion, frameDuration float32, loop bool) *Animation {
+	return &Animation{
+		Frames:        frames,
+		FrameDuration: frameDuration,
+		Loop:          loop,
+	}
+}
+
+func (a *Animation) GetFrame(time float32) *TextureRegion {
+	if len(a.Frames) == 1 {
+		return a.Frames[0]
+	}
+
+	frameIndex := int(time / a.FrameDuration)
+	if a.Loop {
+		return a.Frames[frameIndex%len(a.Frames)]
+	}
+
+	if frameIndex >= len(a.Frames)-1 {
+		return a.Frames[len(a.Frames)-1]
+	}
+	return a.Frames[frameIndex]
+}
+
+func (a *Animation) IsFinished(time float32) bool {
+	if a.Loop {
+		return false
+	}
+	return int(time/a.FrameDuration) >= len(a.Frames)-1
+}
---
+++ src/graphics/shader.go
@@ -0,0 +1,111 @@
+package graphics
+
+import (
+	"fmt"
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+type Shader struct {
+	Program          uint32
+	uniformLocations map[string]int32
+}
+
+func NewShader(vertexSource, fragmentSource string) (*Shader, error) {
+	vertexShader, err := compileShader(vertexSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return nil, fmt.Errorf("vertex shader: %v", err)
+	}
+
+	fragmentShader, err := compileShader(fragmentSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return nil, fmt.Errorf("fragment shader: %v", err)
+	}
+
+	program := gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := make([]byte, logLength+1)
+		gl.GetProgramInfoLog(program, logLength, nil, &log[0])
+
+		return nil, fmt.Errorf("shader link failed: %v", string(log))
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	shader := &Shader{
+		Program:          program,
+		uniformLocations: make(map[string]int32),
+	}
+
+	shader.uniformLocations["u_projection"] = shader.GetUniformLocation("u_projection")
+	shader.uniformLocations["u_texture"] = shader.GetUniformLocation("u_texture")
+
+	return shader, nil
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+	csources, free := gl.Strs(source + "\x00")
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := make([]byte, logLength+1)
+		gl.GetShaderInfoLog(shader, logLength, nil, &log[0])
+
+		return 0, fmt.Errorf("shader compile failed: %v", string(log))
+	}
+
+	return shader, nil
+}
+
+func (s *Shader) Bind() {
+	gl.UseProgram(s.Program)
+}
+
+func (s *Shader) Unbind() {
+	gl.UseProgram(0)
+}
+
+func (s *Shader) GetUniformLocation(name string) int32 {
+	if loc, ok := s.uniformLocations[name]; ok {
+		return loc
+	}
+	loc := gl.GetUniformLocation(s.Program, gl.Str(name+"\x00"))
+	s.uniformLocations[name] = loc
+	return loc
+}
+
+func (s *Shader) SetUniform1i(name string, value int32) {
+	loc := s.GetUniformLocation(name)
+	gl.Uniform1i(loc, value)
+}
+
+func (s *Shader) SetUniform1f(name string, value float32) {
+	loc := s.GetUniformLocation(name)
+	gl.Uniform1f(loc, value)
+}
+
+func (s *Shader) SetUniformMatrix4fv(name string, value *float32) {
+	loc := s.GetUniformLocation(name)
+	gl.UniformMatrix4fv(loc, 1, false, value)
+}
+
+func (s *Shader) Dispose() {
+	gl.DeleteProgram(s.Program)
+}
