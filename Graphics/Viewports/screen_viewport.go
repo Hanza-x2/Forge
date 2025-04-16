cfdb917ebdf5466ab386a8b5ef1b279666577ecd
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2025-04-16 17:09:34 +0200

    Minor changes to screen_viewport.go

--- Graphics/Viewports/screen_viewport.go
+++ Graphics/Viewports/screen_viewport.go
@@ -10,31 +10,39 @@ type ScreenViewport struct {
 	BaseViewport
 }
 
-func NewScreenViewport(screenWidth, screenHeight int) *ScreenViewport {
-	worldWidth := float32(screenWidth)
-	worldHeight := float32(screenHeight)
+func NewScreenViewport(screenWidth, screenHeight int32) *ScreenViewport {
 	viewport := &ScreenViewport{
 		BaseViewport: BaseViewport{
-			WorldWidth:  worldWidth,
-			WorldHeight: worldHeight,
-			Camera:      Graphics.NewCamera(worldWidth, worldHeight),
+			WorldWidth:   float32(screenWidth),
+			WorldHeight:  float32(screenHeight),
+			Camera:       Graphics.NewCamera(float32(screenWidth), float32(screenHeight)),
+			screenX:      0,
+			screenY:      0,
+			screenWidth:  screenWidth,
+			screenHeight: screenHeight,
 		},
 	}
-	viewport.Update(screenWidth, screenHeight, true)
 	return viewport
 }
 
 func (viewport *ScreenViewport) Apply(centerCamera bool) {
 	gl.Viewport(viewport.screenX, viewport.screenY, viewport.screenWidth, viewport.screenHeight)
-	viewport.Camera.Width = viewport.WorldWidth
-	viewport.Camera.Height = viewport.WorldHeight
+	viewport.Camera.Width = float32(viewport.screenWidth)
+	viewport.Camera.Height = float32(viewport.screenHeight)
 	if centerCamera {
-		viewport.Camera.Position = mgl32.Vec2{viewport.WorldWidth / 2, viewport.WorldHeight / 2}
+		viewport.Camera.Position = mgl32.Vec2{
+			float32(viewport.screenWidth) / 2,
+			float32(viewport.screenHeight) / 2,
+		}
 	}
 	viewport.Camera.Update()
 }
 
-func (viewport *ScreenViewport) Update(screenWidth, screenHeight int, centerCamera bool) {
+func (viewport *ScreenViewport) Update(screenWidth, screenHeight int32, centerCamera bool) {
+	viewport.screenX = 0
+	viewport.screenY = 0
+	viewport.screenWidth = screenWidth
+	viewport.screenHeight = screenHeight
 	viewport.WorldWidth = float32(screenWidth)
 	viewport.WorldHeight = float32(screenHeight)
 	viewport.Apply(centerCamera)
