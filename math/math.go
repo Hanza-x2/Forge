e31fad580261342f6a90d45ea294e693af7c0ab8
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2025-04-16 15:42:47 +0200

    Introduced a minimal math package.

---
+++ math/math.go
@@ -0,0 +1,19 @@
+package math
+
+func Lerp(a, b, t float32) float32 {
+	return a + (b-a)*t
+}
+
+func Norm(a, b, t float32) float32 {
+	return (t - a) / (b - a)
+}
+
+func Clamp(value, min, max float32) float32 {
+	if value < min {
+		return min
+	}
+	if value > max {
+		return max
+	}
+	return value
+}
