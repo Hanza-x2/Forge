94007b4497e17025de3475d1602cf49157392596
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2025-04-17 16:05:00 +0200

    Testing a fix for nested hit testing #8.

--- Scene/node.go
+++ Scene/node.go
@@ -304,9 +304,21 @@ func (node *Node) Act(delta float32) {
 }
 
 func (node *Node) Hit(x, y float32) bool {
-	if !node.visible {
+	if !node.visible || node.width <= 0 || node.height <= 0 {
 		return false
 	}
+
 	localX, localY := node.SceneToLocalCoordinates(x, y)
-	return localX >= 0 && localY >= 0 && localX < node.width && localY < node.height
+	if localX < -node.originX || localX > node.width-node.originX ||
+		localY < -node.originY || localY > node.height-node.originY {
+		return false
+	}
+
+	if node.rotation != 0 {
+		invRot := mgl32.HomogRotate2D(-mgl32.DegToRad(node.rotation))
+		unrotated := transformCoordinate(localX+node.originX, localY+node.originY, invRot)
+		localX, localY = unrotated.X()-node.originX, unrotated.Y()-node.originY
+	}
+
+	return localX >= 0 && localX < node.width && localY >= 0 && localY < node.height
 }
